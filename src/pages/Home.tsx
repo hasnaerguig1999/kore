d38d5eb0b1da985041ae435604d9e100f32d7f0b
hasnaerguig1999 <hasnaerguig1999@gmail.com>
2024-03-10 11:38:25 +0000

    la création de promiér page qui contient logo d'application

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,14 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import React from 'react';
+import { IonContent, IonImg,IonPage } from '@ionic/react';
+import '../pages/Home.css'; 
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+    <IonContent style={{ height: '100vh' }} className="ion-background-color">
+      <IonImg src="/assets/img/logo.png" alt="KORE" style={{marginTop: '50%'}} />
+    </IonContent>
+   </IonPage>
   );
 };
 
