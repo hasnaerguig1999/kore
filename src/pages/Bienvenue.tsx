ceb9fd67e4dea52276a8f4923ea8b13c6efcc41f
hasnaerguig1999 <hasnaerguig1999@gmail.com>
2024-03-10 12:32:12 +0000

    la création de dexiéme page qui contient les le buttion Connectez-vous et Créer un compte

--- src/pages/Bienvenue.tsx
+++ src/pages/Bienvenue.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonButton, IonImg, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonText } from '@ionic/react';
+import { IonContent, IonButton, IonRouterLink, IonImg, IonTitle, IonText } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Bienvenue.css';
 
@@ -7,23 +7,19 @@ const BienvenuePage: React.FC = () => {
   return (
     <IonContent className="ion-padding flex flex-col items-center justify-center bg-black text-white">
       <div className="mb-8">
-        <IonImg src="/assets/img/logo.png" alt="KORE" className="w-40 h-40" />
+        <IonImg src="/assets/img/logo.png" alt="KORE" style={{ marginTop: '44%' }} className="w-40 h-40" />
       </div>
-      <IonTitle className="mb-4 text-3xl font-bold">Bienvenue sur KORE</IonTitle> 
-      <IonButton className="inline-flex items-center justify-center whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 mb-4 w-64 rounded bg-custom-yellow py-2 px-4 text-lg font-semibold text-black">        Connectez-vous !
-      </IonButton>
-      <IonButton className="inline-flex items-center justify-center whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 mb-8 w-64 rounded border border-yellow-600 py-2 px-4 text-lg font-semibold">
+      <IonTitle className="text-bienvenue">Bienvenue sur KORE</IonTitle>
+      <IonButton className="button-connecter-vous">Connectez-vous !</IonButton>
+      <IonButton className="creer-compte">
         Créer un compte
       </IonButton>
-      <IonText className="text-xs">
-        By signing up you agree to our{" "}
-        <a href="#" className="underline">
-          terms of service
-        </a>{" "}
-        and{" "}
-        <a href="#" className="underline">
-          privacy policy
-        </a>
+      <IonText className="bysigning">
+        By signing up you agree to our
+
+        <IonRouterLink className="termsservice">
+          terms of service and privacy policy
+        </IonRouterLink>
       </IonText>
     </IonContent>
   );
